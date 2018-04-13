748330add275832cdd653263242bbbb4f403b5e8
Sunghyun <jolacaleb@gmail.com>
2018-04-13 16:04:44 +0900

    - applied sanic framework

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from picamera import PiCamera
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic()
+camera = PiCamera()
+
+
+@app.route('/capture')
+async def capture(request):
+
+    camera.capture('/home/pi/src/photo/image.jpg')
+
+    return json({'hello': 'world'})
 
 
 if __name__ == "__main__":
-    camera = PiCamera()
-
-    camera.capture('/home/pi/src/photo/image.jpg')
+    app.run(host='0.0.0.0', port=8000)
